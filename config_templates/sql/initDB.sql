913da4d663676a5d774f4f71b19060d593bf02b0
blaec <odeskonst@yandex.ru>
2018-07-25 23:03:07 +0300

    h05_01_01 added_cities,_groups,_projects

--- config_templates/sql/initDB.sql
+++ config_templates/sql/initDB.sql
@@ -1,16 +1,43 @@
+DROP TABLE IF EXISTS projects;
 DROP TABLE IF EXISTS users;
-DROP SEQUENCE IF EXISTS user_seq;
+DROP TABLE IF EXISTS groups;
+DROP TABLE IF EXISTS cities;
+DROP SEQUENCE IF EXISTS GLOBAL_SEQ;
 DROP TYPE IF EXISTS user_flag;
+DROP TYPE IF EXISTS group_type;
 
 CREATE TYPE user_flag AS ENUM ('active', 'deleted', 'superuser');
+CREATE TYPE group_type AS ENUM ('REGISTERING', 'CURRENT', 'FINISHED');
 
-CREATE SEQUENCE user_seq START 100000;
+CREATE SEQUENCE global_seq START 100000;
 
-CREATE TABLE users (
-  id        INTEGER PRIMARY KEY DEFAULT nextval('user_seq'),
-  full_name TEXT NOT NULL,
-  email     TEXT NOT NULL,
-  flag      user_flag NOT NULL
+CREATE TABLE cities (
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name        TEXT NOT NULL
 );
 
+CREATE TABLE groups (
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name        TEXT NOT NULL,
+  type        group_type NOT NULL
+);
+
+CREATE TABLE users (
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  full_name   TEXT NOT NULL,
+  email       TEXT NOT NULL,
+  flag        user_flag NOT NULL,
+  city_id     INTEGER NOT NULL,
+  group_id    INTEGER NOT NULL,
+  FOREIGN KEY (city_id) REFERENCES cities(id) ON DELETE CASCADE,
+  FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
+);
 CREATE UNIQUE INDEX email_idx ON users (email);
+
+CREATE TABLE projects (
+  id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+  name        TEXT NOT NULL,
+  desciption  TEXT NOT NULL,
+  group_id    INTEGER NOT NULL,
+  FOREIGN KEY (group_id) REFERENCES groups(id) ON DELETE CASCADE
+);
