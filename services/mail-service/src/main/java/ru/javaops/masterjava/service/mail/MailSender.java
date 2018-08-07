d28fba5b6bef41a7a38f69893a26ca2771621bad
blaec <odeskonst@yandex.ru>
2018-08-07 22:51:52 +0300

    h06_01_01 send_test_email

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -1,12 +1,33 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.mail.DefaultAuthenticator;
+import org.apache.commons.mail.Email;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import ru.javaops.masterjava.config.Configs;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class MailSender {
-    static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) {
-        log.info("Send mail to \'" + to + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+    static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) throws EmailException {
+        Config mail = Configs.getConfig("mail.conf","mail");
+
+        String sendTo = String.join("; ",to.stream().map(Addressee::getEmail).collect(Collectors.toList()));
+        log.info("Send mail to \'" + sendTo + "\' cc \'" + cc + "\' subject \'" + subject + (log.isDebugEnabled() ? "\nbody=" + body : ""));
+
+        Email email = new SimpleEmail();
+        email.setHostName(mail.getString("host"));
+        email.setSmtpPort(mail.getInt("port"));
+        email.setAuthenticator(new DefaultAuthenticator("odeskonst@yandex.ru", mail.getString("password")));
+        email.setSSLOnConnect(mail.getBoolean("useSSL"));
+        email.setFrom("odeskonst@yandex.ru");
+        email.setSubject("TestMail");
+        email.setMsg("This is a test mail ... :-)");
+        email.addTo(sendTo);
+        email.send();
     }
 }
