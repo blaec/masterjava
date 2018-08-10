032a9d8423276ebe23b17e886194c9d6ceb09dd2
blaec <odeskonst@yandex.ru>
2018-08-10 21:25:06 +0300

    h06_01_08 add_to_db_mail_sent_via_web_service_client

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailSender.java
@@ -4,9 +4,13 @@ import com.typesafe.config.Config;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.mail.*;
 import ru.javaops.masterjava.config.Configs;
+import ru.javaops.masterjava.service.dao.SentMailDao;
+import ru.javaops.masterjava.service.model.SentMail;
+import ru.javaops.masterjava.persist.DBIProvider;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.sql.DriverManager;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,31 +19,29 @@ import java.util.stream.Collectors;
 public class MailSender {
     static void sendMail(List<Addressee> to, List<Addressee> cc, String subject, String body) throws EmailException, MalformedURLException {
         Config mail = Configs.getConfig("mail.conf", "mail");
+        MultiPartEmail  email = new MultiPartEmail ();
 
-        String[] sendTo = mailList(to);
-        String[] sendCc = mailList(cc);
-
-        log.info("Send mail to \'" + Arrays.toString(sendTo) + "\' " +
-                "cc \'" + Arrays.toString(sendCc) + "\' " +
-                "subject \'" + subject +
-                (log.isDebugEnabled() ? "\nbody=" + body : ""));
-
-        // Create the attachment
+        // Create and add the attachment
         EmailAttachment attachment = new EmailAttachment();
         attachment.setPath(mail.getString("attachment"));
         attachment.setDisposition(EmailAttachment.ATTACHMENT);
         attachment.setDescription("Picture of " + mail.getString("fromName"));
         attachment.setName(mail.getString("fromName"));
+        email.attach(attachment);
 
-        // Create the attachment with reference any valid URL
+        // Create and add the attachment with reference any valid URL
         EmailAttachment attachmentUrl = new EmailAttachment();
         attachmentUrl.setURL(new URL("http://www.apache.org/images/asf_logo_wide.gif"));
         attachmentUrl.setDisposition(EmailAttachment.ATTACHMENT);
         attachmentUrl.setDescription("Apache logo");
         attachmentUrl.setName("Apache logo");
+        email.attach(attachmentUrl);
 
         // Create the email message
-        MultiPartEmail  email = new MultiPartEmail ();
+        String[] sendTo = mailList(to);
+        String[] sendCc = mailList(cc);
+        String from = "odeskonst@yandex.ru";
+
         email.setHostName(mail.getString("host"));
         email.setSmtpPort(mail.getInt("port"));
         email.setAuthenticator(new DefaultAuthenticator(mail.getString("username"), mail.getString("password")));
@@ -54,12 +56,31 @@ public class MailSender {
             email.addCc(sendCc);
         }
 
-        // add the attachment
-        email.attach(attachment);
-        email.attach(attachmentUrl);
-
         // send the email
         email.send();
+
+        log.info("Send mail to \'" + Arrays.toString(sendTo) + "\' " +
+                "cc \'" + Arrays.toString(sendCc) + "\' " +
+                "subject \'" + subject +
+                (log.isDebugEnabled() ? "\nbody=" + body : ""));
+
+        // Save results to db
+        SentMail sentMail = new SentMail(
+                email.isBoolHasAttachments(), from, mail.getString("fromName"), subject, body,
+                String.join(",",sendTo), String.join(",",sendCc));
+
+        Config db = Configs.getConfig("persist.conf","db");
+        DBIProvider.init(() -> {
+            try {
+                Class.forName("org.postgresql.Driver");
+            } catch (ClassNotFoundException e) {
+                throw new IllegalStateException("PostgreSQL driver not found", e);
+            }
+            return DriverManager.getConnection(db.getString("url"), db.getString("user"), db.getString("password"));
+        });
+
+        SentMailDao dao = DBIProvider.getDao(SentMailDao.class);
+        dao.insert(sentMail);
     }
 
     private static String[] mailList(List<Addressee> input) {
