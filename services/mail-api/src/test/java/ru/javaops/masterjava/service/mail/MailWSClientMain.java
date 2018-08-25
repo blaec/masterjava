8d36cb5f4bbdf94381f777a24db86c6f4ba2e32e
blaec <odeskonst@yandex.ru>
2018-08-25 18:14:34 +0300

    h07_01_01 fix_wsdl_files

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -5,7 +5,7 @@ import com.google.common.collect.ImmutableSet;
 public class MailWSClientMain {
     public static void main(String[] args) {
         MailWSClient.sendToGroup(
-                ImmutableSet.of(new Addressee("To <masterjava@javaops.ru>")),
-                ImmutableSet.of(new Addressee("Copy <masterjava@javaops.ru>")), "Subject", "Body");
+                ImmutableSet.of(new Addressee("To <blaec@yandex.ru>")),
+                ImmutableSet.of(new Addressee("Copy <blaec@yandex.ru>")), "Subject", "Body");
     }
 }
