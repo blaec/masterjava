68892992785d9040766ed171df3ef9d4932e1fe1
blaec <odeskonst@yandex.ru>
2018-09-01 19:16:17 +0300

    m08_08_05 soap_exceptions

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
@@ -14,9 +14,10 @@ public class MailServicePublisher {
     public static void main(String[] args) {
         DBITestProvider.initDBI();
 
-        Endpoint endpoint = Endpoint.create(new MailServiceImpl());
+    Endpoint endpoint = Endpoint.create(new MailServiceImpl());
         List<Source> metadata = ImmutableList.of(
-                new StreamSource(Configs.getConfigFile("wsdl/mailService.wsdl")));
+                new StreamSource(Configs.getConfigFile("wsdl/mailService.wsdl")),
+                new StreamSource(Configs.getConfigFile("wsdl/common.xsd")));
 
         endpoint.setMetadata(metadata);
         endpoint.publish("http://localhost:8080/mail/mailService");
