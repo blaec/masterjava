1e7eee5fea3a79d469ccd9ec614e191181de5491
blaec <odeskonst@yandex.ru>
2018-09-07 23:06:21 +0300

    h08_01_01 MTOM port

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -5,15 +5,26 @@ import com.google.common.collect.ImmutableSet;
 import lombok.extern.slf4j.Slf4j;
 import ru.javaops.web.WebStateException;
 
+import javax.activation.DataHandler;
+import java.io.File;
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
 public class MailWSClientMain {
-    public static void main(String[] args) throws WebStateException {
+    public static void main(String[] args) throws WebStateException, MalformedURLException {
+        List<Attachment> attachments = ImmutableList.of(
+                new Attachment(new DataHandler(new File("config_templates/version.html").toURI().toURL())));
+
         String state = MailWSClient.sendToGroup(
                 ImmutableSet.of(new Addressee("To <blaec@yandex.ru>")),
-                ImmutableSet.of(new Addressee("Copy <blaec@yandex.ru>")), "Subject", "Body", null);
+                ImmutableSet.of(new Addressee("Copy <blaec@yandex.ru>")), "Subject", "Body", attachments);
+
+        GroupResult groupResult = MailWSClient.sendBulk(ImmutableSet.of(
+                new Addressee("Мастер Java <blaec@yandex.ru>"),
+                new Addressee("Bad Email <bad_email.ru>")), "Bulk mail subject", "Bulk mail body", attachments);
+        System.out.println("\nBulk mail groupResult:\n" + groupResult);
         System.out.println(state);
     }
 }
--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/MailServiceImpl.java
@@ -3,12 +3,14 @@ package ru.javaops.masterjava.service.mail;
 import ru.javaops.web.WebStateException;
 
 import javax.jws.WebService;
+import javax.xml.ws.soap.MTOM;
 import java.util.List;
 import java.util.Set;
 
 @WebService(endpointInterface = "ru.javaops.masterjava.service.mail.MailService", targetNamespace = "http://mail.javaops.ru/"
 //          , wsdlLocation = "WEB-INF/wsdl/mailService.wsdl"
 )
+@MTOM
 public class MailServiceImpl implements MailService {
     public String sendToGroup(Set<Addressee> to, Set<Addressee> cc, String subject, String body, List<Attachment> attaches) throws WebStateException {
         return MailSender.sendToGroup(to, cc, subject, body);
