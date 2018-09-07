e96bbdf2ce0a4045fe3710bc931d0f55ed19e73c
blaec <odeskonst@yandex.ru>
2018-09-07 20:20:53 +0300

    h08_01_00 Attachment class created

--- services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
+++ services/mail-api/src/test/java/ru/javaops/masterjava/service/mail/MailWSClientMain.java
@@ -1,15 +1,19 @@
 package ru.javaops.masterjava.service.mail;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import lombok.extern.slf4j.Slf4j;
 import ru.javaops.web.WebStateException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Slf4j
 public class MailWSClientMain {
     public static void main(String[] args) throws WebStateException {
         String state = MailWSClient.sendToGroup(
                 ImmutableSet.of(new Addressee("To <blaec@yandex.ru>")),
-                ImmutableSet.of(new Addressee("Copy <blaec@yandex.ru>")), "Subject", "Body");
+                ImmutableSet.of(new Addressee("Copy <blaec@yandex.ru>")), "Subject", "Body", null);
         System.out.println(state);
     }
 }
